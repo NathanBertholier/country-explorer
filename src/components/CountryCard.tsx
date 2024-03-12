1601d86de4e0e722a81e82f21bca1a49b0d579dc
Nathan <nathan.bertholier@gmail.com>
2024-03-12 18:36:41 +0100

    add CountryCard

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,19 @@
+import { Country } from "@/types/types";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import dynamic from 'next/dynamic'; // Composant LeafletMap chargé dynamiquement 
+const Map = dynamic(() => import('@/components/Map'), { ssr: false });
+
+export default function ContryCard({ country }: { country: Country }) {
+    return (<div>
+        <Card className="py-4" >
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <p className="text-tiny uppercase font-bold">{country.name}</p>
+                <small className="text-default-500">{country.region.value}</small>
+                <h4 className="font-bold text-large">{country.capitalCity}</h4>
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
+                <Map country={country} />
+            </CardBody>
+        </Card>
+    </div>)
+}
---
+++ src/components/CountryList.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
+import { Country } from "@/types/types";
+import { Pagination } from "@nextui-org/react";
+
+
+
+export default function CountryList() {
+    const [countries, setcountries] = useState<Country[]>([])
+    const [page, setpage] = useState({ total: 1, initialPage: 1 })
+    useEffect(() => {
+        getCountries(1)
+    }, [])
+
+    const getCountries = async (page: number) => {
+        const res = await fetch("https://api.worldbank.org/v2/country?format=json&language=fr&page=" + page)
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+        const data = await res.json()
+        console.log(data[1])
+        setpage({ total: data[0].pages, initialPage: data[0].page })
+        setcountries(data[1])
+    }
+
+    const countryCardList = countries.filter((country) => country.capitalCity).map((country) => <CountryCard key={country.id} country={country} />)
+
+    return (
+        <>
+            <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+                {countryCardList}
+            </div>
+            <Pagination total={page.total} initialPage={page.initialPage} onChange={(index) => getCountries(index)} />
+
+        </>
+    )
+}
